789c784d756624ceec46a65a720ccb93ee8cd06f
phoebekn <phoebenyaruri@gmail.com>
2023-03-23 13:59:52 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints number between 0 to 9
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
