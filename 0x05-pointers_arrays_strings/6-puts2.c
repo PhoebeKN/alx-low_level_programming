98e17f2176c7c16d864509ef6d80e73adc0cb994
phoebekn <phoebenyaruri@gmail.com>
2023-03-27 03:31:37 -0700

    character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: input
+ * Return: always 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
