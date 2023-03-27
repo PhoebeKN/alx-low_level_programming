992a762222c7e7ffd964fbf642b70aa64d52f223
phoebekn <phoebenyaruri@gmail.com>
2023-03-27 10:13:36 -0700

    solve the problem

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * _strcpy - string copies
  * @dest: destination array
@@ -11,8 +10,6 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	if (dest == NULL)
-		return (NULL);
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
 	return (dest);
